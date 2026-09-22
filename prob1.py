be5d92eca00339170ca4d7408c3c978a769637fb
Brentt Ethann Yap <bryap@carc.pshs.edu.ph>
2026-09-22 21:38:59 +0800

    Enhance age validation logic in prob1.py

--- prob1.py
+++ prob1.py
@@ -6,10 +6,14 @@
 try:
     student_age = int(input("Enter your age: "))
     # Validating the range of student age
-    if 12 <= student_age <= 18:
-        print("Valid Age.")
+    if student_age < 0:
+        print("Invalid Age. Please enter a whole number. ")
+
     else:
-        print("Invalid Age. Age must be from 12 to 18. ")
+        if 12 <= student_age <= 18:
+            print("Valid Age.")
+        else:
+            print("Invalid Age. Age must be from 12 to 18. ")
 # Data Type Validator displaying wrong data type
 except ValueError:
     print("Invalid Age. Please enter a whole number. ")
